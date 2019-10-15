3d4cc1de63fe14b206a352908b6296802d65edaf
Santiago Trini <santiagotrini@gmail.com>
2019-10-14 23:18:13 -0300

    agrega respuestas

--- README.md
+++ README.md
@@ -12,9 +12,14 @@ Ejercicio para Proyecto Informatico I.
 - [x] Realizar el primer commit y agregar el remoto de GitHub, pushear.
 - [x] Instalar paquetes de npm: express y mongoose.
 - [x] Crear un archivo `index.js` y poner codigo para crear una app de Express.
-- [ ] Instalar nodemon con npm y cambiar la seccion scripts del `package.json`.
-- [ ] Conectar a una base de datos de MongoDB.
-- [ ] Configurar el motor y la carpeta de vistas para la app de Express.
-- [ ] Crear un modelo para las preguntas.
-- [ ] Crear una vista para el home (`index.pug`). Usar una lista ordenada para las preguntas.
-- [ ] Escribir una ruta para la home page que haga una query a la base de datos y lleve las preguntas a la vista.
+- [x] Instalar nodemon con npm y cambiar la seccion scripts del `package.json`.
+- [x] Conectar a una base de datos de MongoDB.
+- [x] Configurar el motor y la carpeta de vistas para la app de Express.
+- [x] Crear un modelo para las preguntas.
+- [x] Crear una vista para el home (`index.pug`). Usar una lista ordenada para las preguntas.
+- [x] Escribir una ruta para la home page que haga una query a la base de datos y lleve las preguntas a la vista.
+- [ ] Crear un modelo para las respuestas.
+- [ ] Modificar el modelo de las preguntas para que pueda hacer referencia a las respuestas.
+- [ ] Modificar el formulario de nueva pregunta para poder cargar las respuestas tambien.
+- [ ] Mostrar las preguntas con sus respuestas.
+- [ ] Mostrar cuando el jugador acierta o falla en una pregunta.
--- index.js
+++ index.js
@@ -20,10 +20,19 @@ mongoose
 
 // schema y modelo de pregunta
 const QuestionSchema = new mongoose.Schema({
-  question: { type: String, required: true }
+  title: { type: String, required: true },
+  answers: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Answer' }]
 });
 
 const Question = mongoose.model('Question', QuestionSchema);
+
+const AnswerSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  isRight: { type: Boolean, default: false },
+  question: { type: mongoose.Schema.Types.ObjectId, ref: 'Question' }
+});
+
+const Answer = mongoose.model('Answer', AnswerSchema);
 
 // middleware
 app.use(express.json());
@@ -36,16 +45,52 @@ app.set('views', './views');
 // rutas
 app.post('/question', (req, res) => {
   let question = new Question({
-    question: req.body.question
+    _id: new mongoose.Types.ObjectId(),
+    title: req.body.question
   });
+  let answerA = new Answer({
+    title: req.body.answerA,
+    isRight: req.body.checkboxA,
+    question: question._id
+  });
+  let answerB = new Answer({
+    title: req.body.answerB,
+    isRight: req.body.checkboxB,
+    question: question._id
+  });
+  let answerC = new Answer({
+    title: req.body.answerC,
+    isRight: req.body.checkboxC,
+    question: question._id
+  });
+  let answerD = new Answer({
+    title: req.body.answerD,
+    isRight: req.body.checkboxD,
+    question: question._id
+  });
+  question.answers.push(answerA);
+  question.answers.push(answerB);
+  question.answers.push(answerC);
+  question.answers.push(answerD);
   question.save(err => {
+    answerA.save();
+    answerB.save();
+    answerC.save();
+    answerD.save();
     res.redirect('/');
   });
 });
 
 app.get('/', (req, res) => {
-  Question.find((err, questions) => {
+  Question.find().populate('answers').exec((err, questions) => {
     res.render('index', { questions: questions });
+  });
+});
+
+// para testear nomas
+app.get('/api/questions', (req, res) => {
+  Question.find().populate('answers').exec((err, questions) => {
+    res.json(questions);
   });
 });
 
